b85fa64aa79634fa84ebab9baa0ad8b18b8c06af
Paul Hannon <hancsu@live.com>
2020-06-18 19:03:14 +0100

    write migration for maturity categories table

---
+++ src/BeltsAndLeaders.Server.Data/Migrations/CreateMaturityCategoriesTable20200618185900.cs
@@ -0,0 +1,22 @@
+using FluentMigrator;
+
+namespace BeltsAndLeaders.Server.Data.Migrations
+{
+    [Migration(20200618185900)]
+    public class CreateMaturityCategoriesTable20200618185900 : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("MaturityCategories")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Name").AsFixedLengthString(255)
+                .WithColumn("CreatedAt").AsInt64()
+                .WithColumn("UpdatedAt").AsInt64().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("MaturityCategories");
+        }
+    }
+}
