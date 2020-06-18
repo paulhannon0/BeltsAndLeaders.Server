02f70deb6790f6ee2451d3ca765b6af425593924
Paul Hannon <hancsu@live.com>
2020-06-18 18:46:28 +0100

    write migration for users table

---
+++ src/BeltsAndLeaders.Server.Data/Migrations/CreateUsersTable20200618184300.cs
@@ -0,0 +1,27 @@
+using FluentMigrator;
+
+namespace BeltsAndLeaders.Server.Data.Migrations
+{
+    [Migration(20200618184300)]
+    public class CreateUsersTable20200618184300 : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Name").AsFixedLengthString(255)
+                .WithColumn("Email").AsFixedLengthString(255)
+                .WithColumn("MaturityLevel").AsByte()
+                .WithColumn("Belt").AsFixedLengthString(255)
+                .WithColumn("SpecialistArea").AsFixedLengthString(255).Nullable()
+                .WithColumn("ChampionStartDate").AsInt64()
+                .WithColumn("CreatedAt").AsInt64()
+                .WithColumn("UpdatedAt").AsInt64().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Users");
+        }
+    }
+}
