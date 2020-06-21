2a3c6ceacebc207525a3c6a1c4242df902f5cd4d
Paul Hannon <hancsu@live.com>
2020-06-21 20:41:00 +0100

    migration for achievements table

---
+++ src/BeltsAndLeaders.Server.Data/Migrations/CreateAchievementsTable2020062100.cs
@@ -0,0 +1,25 @@
+using FluentMigrator;
+
+namespace BeltsAndLeaders.Server.Data.Migrations
+{
+    [Migration(2020062100)]
+    public class CreateAchievementsTable20200618184300 : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Achievements")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("UserId").AsInt64().ForeignKey("Users", "Id")
+                .WithColumn("MaturityLevelId").AsInt64().ForeignKey("MaturityLevels", "Id")
+                .WithColumn("AchievementDate").AsInt64()
+                .WithColumn("Comment").AsFixedLengthString(1000)
+                .WithColumn("CreatedAt").AsInt64()
+                .WithColumn("UpdatedAt").AsInt64().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Achievements");
+        }
+    }
+}
