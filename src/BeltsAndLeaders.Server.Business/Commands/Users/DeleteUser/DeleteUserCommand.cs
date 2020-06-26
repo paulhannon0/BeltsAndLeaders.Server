13df27b9d8c5ff9f6e2094ed7797dcad9e9c7e81
James Sunley <James.Sunley@sage.com>
2020-06-26 15:27:43 +0100

    Added DeleteUserCommand

---
+++ src/BeltsAndLeaders.Server.Business/Commands/Users/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,32 @@
+﻿using BeltsAndLeaders.Server.Business.Models.Users.DeleteUser;
+using BeltsAndLeaders.Server.Common.Exceptions;
+using BeltsAndLeaders.Server.Data.Repositories;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BeltsAndLeaders.Server.Business.Commands.Users.DeleteUser
+{
+    public class DeleteUserCommand : IDeleteUserCommand
+    {
+        private readonly IUsersRepository usersRepository;
+
+        public DeleteUserCommand(IUsersRepository usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+
+        public async Task<ulong> ExecuteAsync(DeleteUserCommandRequestModel commandRequest)
+        {
+            var user = await this.usersRepository.GetAsync(commandRequest.Id);
+
+            if (user == null)
+            {
+                throw new HttpException(HttpStatusCode.NotFound, $"User (ID: {commandRequest.Id}) cannot be found.");
+            }
+
+            await this.usersRepository.DeleteAsync(commandRequest.Id);
+
+            return commandRequest.Id;
+        }
+    }
+}
---
+++ src/BeltsAndLeaders.Server.Business/Models/Users/DeleteUser/DeleteUserCommandRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeltsAndLeaders.Server.Business.Models.Users.DeleteUser
+{
+    class DeleteUserCommandRequestModel
+    {
+    }
+}
