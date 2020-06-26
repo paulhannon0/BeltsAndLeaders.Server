f70dc2e8cf751483456ddae2f73806752107409c
James Sunley <James.Sunley@sage.com>
2020-06-26 15:33:29 +0100

    Added DeleteUserCommandRequestModel

--- src/BeltsAndLeaders.Server.Business/Models/Users/DeleteUser/DeleteUserCommandRequestModel.cs
+++ src/BeltsAndLeaders.Server.Business/Models/Users/DeleteUser/DeleteUserCommandRequestModel.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BeltsAndLeaders.Server.Business.Models.Users.DeleteUser
+﻿namespace BeltsAndLeaders.Server.Business.Models.Users.DeleteUser
 {
-    class DeleteUserCommandRequestModel
+    public class DeleteUserCommandRequestModel
     {
+        public ulong Id { get; set; }
     }
 }
