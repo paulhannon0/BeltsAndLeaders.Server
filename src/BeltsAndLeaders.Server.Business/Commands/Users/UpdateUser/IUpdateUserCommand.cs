fec26966f17769532d338610d67b08e606d41838
James Sunley <James.Sunley@sage.com>
2020-07-09 15:38:24 +0100

    Added update user

---
+++ src/BeltsAndLeaders.Server.Business/Commands/Users/UpdateUser/IUpdateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using BeltsAndLeaders.Server.Business.Models.Users.UpdateUser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeltsAndLeaders.Server.Business.Commands.Users.UpdateUser
+{
+    public interface IUpdateUserCommand
+    {
+        public interface IUpdateUserCommand : ICommand<UpdateUserCommandRequestModel, ulong> { }
+    }
+}
